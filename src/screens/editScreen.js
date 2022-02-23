53dcb3bd7bc75866eda3afa27e70e47aeb5e5f87
michael clark <michaelclark@michaels-Air.fios-router.home>
2022-02-23 12:40:20 -0500

    added update functions for edit screen and some navigation

--- src/screens/editScreen.js
+++ src/screens/editScreen.js
@@ -1,15 +1,25 @@
-import React from "react";
-import { View, Text, StyleSheet, } from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet } from 'react-native';
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 
 const editScreen = ({ navigation }) => {
-    return (
-        <View>
-            <Text>
-                Edit Screen - {navigation.getParam('id')}
-            </Text>
-        </View>
+    const id = navigation.getParam('id');
+    const { state, editBlogPost } = useContext(Context);
+
+    const blogPost = state.find((blogPost) => blogPost.id === id
     );
+
+    return (
+    <BlogPostForm 
+    initalValues={{ title: blogPost.title, content: blogPost.content }}
+    onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+        // .pop() sends you back to previous screen
+    }}
+    />
+    )
 };
 
 const styles = StyleSheet.create({});
